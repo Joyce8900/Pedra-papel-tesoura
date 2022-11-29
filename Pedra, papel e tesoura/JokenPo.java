3177ffbcfeed2564fa2ac216d48cabd94d212be8
Joyce <joycesantos8900@gmail.com>
2022-11-28 22:56:16 -0300

    inclementação da função winnerRound

--- Pedra, papel e tesoura/JokenPo.java
+++ Pedra, papel e tesoura/JokenPo.java
@@ -1,18 +1,63 @@
 import java.util.Scanner;
+import java.util.Random;
 
-public record JokenPo(Player user,Player IA,int rounds)
+public record JokenPo(
+    Player user,
+    Player IA,
+    int rounds
+)
 {
     
     public void toPlay(){
-        System.out.println("\n ***** Bem vindo *****, " + user.getName() +" ******\n");
-        int choiceUser =  choiceUser();
-        if (choiceUser < 1 || choiceUser > 3){
+        for (int i = 1; i <= rounds; i=i+1){
+            System.out.println("\n ***** Bem vindo *****, " + user.getName() +" ******\n");
+            int choiceUser =  choiceUser();
+            if (choiceUser < 1 || choiceUser > 3){
             System.out.println("\n Jogada invalida!");
             System.out.println("\n Ponto para " + IA.getName() +"\n");
             IA.incrementScore();
+            continue;
         
         }
-} 
+        int choiceIA = choiceIA();
+        
+        System.out.print("\n"+ choiceUser + "X"+ choiceIA);
+        
+        int result =  choiceUser - choiceIA; 
+        
+        winnerRound(result);
+        
+        }
+        
+        
+}  private void winnerRound(int result){
+        String winnerRound;
+        if (result = 0){
+            winnerRound = ("EMPATE");
+        }
+        else if{
+            if (result = -1 || result = 2){
+                IA().incrementScore();
+                winnerRound = IA().getName();
+        }
+            else if{
+                user.incrementScore();
+                winnerRound = user.getName();
+            }
+            
+        }
+
+
+
+}
+
+
+ private int choiceIA(){
+        Random random = new Random();
+        return random.nextInt(3)+1;
+
+}
+
     private int choiceUser(){
         Scanner scan = new Scanner(System.in);
         System.out.println("1 - PEDRA");
